6f069d93f7fa55d9f48508dfe3c3397cc7579e94
FreSauce <24shardul@gmail.com>
2021-05-22 22:08:32 +0530

    added manga details page

--- src/Components/MangaDetails.js
+++ src/Components/MangaDetails.js
@@ -4,7 +4,6 @@ import { getManga } from "../store/actions";
 import { useEffect } from "react";
 import { Container, Col, Row, Card } from "react-bootstrap";
 import classes from "./mangaTitles.module.css";
-import Markdown from "react-markdown";
 
 const MangaDetails = () => {
   const { id } = useParams();
@@ -16,23 +15,52 @@ const MangaDetails = () => {
   }, [dispatch, id]);
 
   console.log(mangaDetails);
+  if (!mangaDetails || mangaDetails.id !== id) {
+    return null;
+  }
   return (
     <Container fluid>
       <Row>
-          <Col xs={12} md={12} lg={12} className="my-4">
-            <Card className={"shadow border-0 h-100 " + classes.card}>
-              <Card.Header className="bg-transparent border-0">
-                  <Card.Title>{mangaDetails.title}</Card.Title>
-              </Card.Header>
-              <Card.Body>
-                <Card.Text>
-                  <Markdown
-                    children={mangaDetails.description}
-                  />
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
+        <Col xs={12} md={8} lg={8} className="my-4">
+          <Card className={"shadow border-0 h-100 " + classes.card}>
+            <Card.Header className="bg-transparent border-0">
+              <Card.Title>{mangaDetails.title}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Card.Text>
+                {mangaDetails.description.substring(
+                  0,
+                  mangaDetails.description.indexOf("[")
+                )}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={8} lg={8} className="my-4">
+          <Card className={"shadow border-0 h-100 " + classes.card}>
+            <Card.Header className="bg-transparent border-0">
+              <Card.Title>Chapters</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              {mangaDetails.chapList.map((chap) => (
+                <Row>
+                    <Col xs={12} md={8} lg={8} className="my-1">
+                  <Card className={"shadow border-0 h-100 " + classes.card}>
+                      <Card.Body className="bg-transparent border-0">
+
+                      <Card.Text>
+                          Chapter {chap.chapter} {chap.title}
+                        </Card.Text>
+                      </Card.Body>
+                  </Card>
+                    </Col>
+                </Row>
+              ))}
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
